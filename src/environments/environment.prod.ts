8a3e77e95820c4f8f723076183dad3c484b098c2
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-05-29 11:00:49 -0500

    Configuration Firebase on proyect

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+
+export const environment = {
+    firebase: {
+      projectId: 'siboadvance',
+      appId: '1:512086210269:web:f608572c53576a04730d34',
+      storageBucket: 'siboadvance.appspot.com',
+      apiKey: 'AIzaSyAviS7jY2rppr17NdJgpVrgVqDkbCEQ8CY',
+      authDomain: 'siboadvance.firebaseapp.com',
+      messagingSenderId: '512086210269',
+    }, production: true,
+           //   urlApiUsers: 'https://localhost:7056/User',
+           //   urlApiDeliveries: 'https://localhost:7245/api/Delivery',
+           //   urlApiCampus: 'https://localhost:7245/',
+           //   urlApiCourses: 'https://localhost:7245/api/Course',
+      };
+  
