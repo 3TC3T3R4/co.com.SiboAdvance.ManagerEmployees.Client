b53825e7444167034a4d6c0ef6de1f468cd56a18
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-05-29 15:23:12 -0500

    Created bussiness face in the proyect

---
+++ src/bussiness/repositories/user/user.repository.ts
@@ -0,0 +1,8 @@
+import { Observable } from "rxjs";
+import { CreateUserCommand } from "src/domain/commands/user/newUserCommands";
+
+export abstract class UserRepository {
+    abstract createUserAsync(newUserCommand: CreateUserCommand): Observable<string>;
+  
+    
+}
